4ec441f14b9359ac9b64fe0065cc1a9836e7c2b1
astra.celestine <109295196+astra-the-boop@users.noreply.github.com>
2026-06-26 14:56:34 +0700

    pino

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("piano-btn").addEventListener('click', function(){
+        document.getElementById("piano").classList.add("falling");
+    })
+})
